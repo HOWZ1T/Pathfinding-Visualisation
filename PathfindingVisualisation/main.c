bc3764aff9f825e140a68050c11512073571e4e2
HOWZ1T <dylan.d.randall@gmail.com>
2019-07-08 15:06:14 -0300

    minor work on learning to render textures.

--- PathfindingVisualisation/main.c
+++ PathfindingVisualisation/main.c
@@ -25,6 +25,15 @@ static struct {
 		SDL_Renderer* renderer;
 	} screen;
 
+	// definition for a drawing surface
+	struct {
+		SDL_Surface* surface;
+		SDL_Texture* texture;
+		Uint32 rmask, gmask, bmask, amask;
+		unsigned int width;
+		unsigned int height;
+	} canvas;
+
 	// define methods
 	void (*init)(void);
 	void (*quit)(void);
@@ -38,6 +47,7 @@ static struct {
 		NULL,
 		NULL
 	},
+	{NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL},
 	init,
 	quit,
 	handle_events,
@@ -51,6 +61,7 @@ void init(void) {
 		exit(1);
 	}
 
+	// creating window and renderer
 	unsigned int w = App.screen.width;
 	unsigned int h = App.screen.height;
 
@@ -64,21 +75,78 @@ void init(void) {
 	App.screen.renderer = SDL_CreateRenderer(App.screen.window, -1, 
 		SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
+	// checking that the window and renderer were actually created
+	if (App.screen.window == NULL)
+	{
+		printf("SDLError -> %s\n", SDL_GetError());
+		exit(2);
+	}
+
+	if (App.screen.renderer == NULL)
+	{
+		printf("SDLError -> %s\n", SDL_GetError());
+		exit(3);
+	}
+
+	// creating the surface
+	// setting up RGBA bitmasks based on SDL byteorder
+	Uint32 rmask = 0x0, gmask = 0x0, bmask = 0x0, amask = 0x0;
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+	rmask = 0xff000000;
+	gmask = 0x00ff0000;
+	bmask = 0x0000ff00;
+	amask = 0x000000ff;
+#else
+	rmask = 0x000000ff;
+	gmask = 0x0000ff00;
+	bmask = 0x00ff0000;
+	amask = 0xff000000;
+#endif
+
+	App.canvas.rmask = rmask;
+	App.canvas.gmask = gmask;
+	App.canvas.bmask = bmask;
+	App.canvas.amask = amask;
+
+	App.canvas.width = App.screen.width;
+	App.canvas.height = App.screen.height;
+	
+	// parameters are as follows: flags, width, height, bit depth, rgba masks...
+	App.canvas.surface = SDL_CreateRGBSurface(0, App.canvas.width, App.canvas.height, 32, rmask, gmask, bmask, amask);
+	if (App.canvas.surface == NULL)
+	{
+		printf("SDL_Error -> %s\n", SDL_GetError());
+		exit(4);
+	}
+
+	// creating the initial texture
+	App.canvas.texture = SDL_CreateTextureFromSurface(App.screen.renderer, App.canvas.surface);
+	if (App.canvas.texture == NULL)
+	{
+		printf("SDL_Error -> %s\n", SDL_GetError());
+		exit(5);
+	}
+
 	App.running = SDL_TRUE;
 	printf("initialized.\n");
 }
 
 void quit(void) {
 	printf("quiting app...\n");
+
+	// freeing memory
+	SDL_FreeSurface(App.canvas.surface);
+	SDL_DestroyTexture(App.canvas.texture);
 	SDL_DestroyRenderer(App.screen.renderer);
 	SDL_DestroyWindow(App.screen.window);
 
+	App.canvas.surface = NULL;
+	App.canvas.texture = NULL;
 	App.screen.window = NULL;
 	App.screen.renderer = NULL;
 
 	SDL_Quit();
 	App.running = SDL_FALSE;
-	printf("app closed.\n");
 }
 
 void handle_events(SDL_Event* event_ptr)
@@ -98,17 +166,31 @@ int main(int argc, char* argv[])
 {
 	App.init();
 
-	printf("app running...");
+	printf("app running...\n");
 	SDL_Event event;
 	while (App.running)
 	{
 		App.handle_events(&event);
 
 		SDL_RenderClear(App.screen.renderer);
+
+		// TODO: setup turtle as the character to pathfind
+		// TODO: draw objects directly to the renderer
+
+		// drawing surface
+		SDL_FillRect(App.canvas.surface, NULL, SDL_MapRGBA(App.canvas.surface->format, 255, 100, 150, 255));
+		
+		// updating texture from the changed surface
+		SDL_DestroyTexture(App.canvas.texture);
+		App.canvas.texture = SDL_CreateTextureFromSurface(App.screen.renderer, App.canvas.surface);
+
+		// renderering to the screen
+		SDL_RenderCopy(App.screen.renderer, App.canvas.texture, NULL, NULL);
 		SDL_RenderPresent(App.screen.renderer);
 	}
 
 	App.quit();
+	printf("app closed.\n");
 
 	return 0;
 }
